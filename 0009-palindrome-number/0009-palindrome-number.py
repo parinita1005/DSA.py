3f1eed27d45e21a85c054695992988aabc763b2a
parinita1005 <parinita1005@gmail.com>
2026-08-18 23:42:55 +0530

    Time: 9 ms (64.05%), Space: 12.5 MB (18.33%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        num = x
+        result = 0
+        while num>0:
+  
+            ld = num%10
+            result=(result*10)+ld
+            num=num//10
+
+        return x == result
+        # print(isPalindrome(x))     
